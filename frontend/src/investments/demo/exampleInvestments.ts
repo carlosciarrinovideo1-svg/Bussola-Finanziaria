eb2ce754689f9e91d0f01e259fafc8ecfa3fc915
Carlo <carlosciarrinovideo1@gmail.com>
2026-08-05 15:57:09 +0100

    BF-0091: add multiple investment demo series

--- frontend/src/investments/demo/exampleInvestments.ts
+++ frontend/src/investments/demo/exampleInvestments.ts
@@ -12,4 +12,20 @@ export const exampleInvestments: Investment[] = [
     annualGrossRate: 5,
     capitalization: "compound",
   },
+  {
+    id: "rental-demo",
+    name: "Affitto immobiliare",
+    initialCapital: 150000,
+    durationMonths: 24,
+    annualGrossRate: 4,
+    capitalization: "simple",
+  },
+  {
+    id: "flip-demo",
+    name: "Flip immobiliare",
+    initialCapital: 80000,
+    durationMonths: 12,
+    annualGrossRate: 8,
+    capitalization: "simple",
+  },
 ];
